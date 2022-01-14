151ce739d5deb1f78b5db626bcddcf6cb756d3fe
kdbiertje <48162904+kdbiertje@users.noreply.github.com>
2022-01-14 13:24:10 +0100

    added bounce

--- Assets/Scripts/Kevin Scripts/Movement.cs
+++ Assets/Scripts/Kevin Scripts/Movement.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.IO;
 
 public class Movement : MonoBehaviour
 {
@@ -17,7 +14,14 @@ public class Movement : MonoBehaviour
 
     public float hangTime = .1f;
     private float _hangCounter;
-    public float moveRightLeft;
+    private float _moveRightLeft;
+
+    private bool _touchingWall;
+    public Transform wallCheck;
+    public LayerMask wallLayer;
+    private bool _wallJumping;
+    public float pushOutFromWallForce;
+
 
 
     // Start is called before the first frame update
@@ -31,29 +35,29 @@ public class Movement : MonoBehaviour
     {
         _moveInput = Input.GetAxisRaw("Horizontal");
 
+        //stores the value of the direction player last moved
         if (_moveInput > 0 && _isGrounded)
         {
-            moveRightLeft = moveSpeed;
+            _moveRightLeft = moveSpeed;
         }
         else if (_moveInput < 0 && _isGrounded)
         {
-            moveRightLeft = -moveSpeed;
+            _moveRightLeft = -moveSpeed;
         }
-        
-        //Movement horizontally
-        if (_isGrounded)
-        {
-            _rb2D.velocity = new Vector2(_moveInput * moveSpeed, _rb2D.velocity.y);
-        }
-        
+
         //check if player is on the ground
         _isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, 0.1f, whatIsGround) ||
                      Physics2D.OverlapCircle(groundCheckPoint2.position, 0.1f, whatIsGround);
 
-        //if the player jumps a bit to late he will still jump
+        //check if player is touching a wall
+        _touchingWall = Physics2D.OverlapCircle(wallCheck.position, 0.15f, wallLayer);
+
+        //if the player jumps a bit to late he will still jump and movement only when grounded cant change mid air
         if (_isGrounded)
         {
             _hangCounter = hangTime;
+            _rb2D.velocity = new Vector2(_moveInput * moveSpeed, _rb2D.velocity.y);
+            _wallJumping = false;
         }
         else
         {
@@ -63,12 +67,23 @@ public class Movement : MonoBehaviour
         //jump in the air
         if (Input.GetButton("Jump") && _hangCounter > 0f)
         {
-            _rb2D.velocity = new Vector2(moveRightLeft, jumpForce);
+            _rb2D.velocity = new Vector2(_moveRightLeft, jumpForce);
         }
+
         //jump reduced in height if jump button is let go before reaching max height
         if (Input.GetButtonUp("Jump") && _rb2D.velocity.y > 0)
         {
-            _rb2D.velocity = new Vector2(moveRightLeft, _rb2D.velocity.y * .5f);
+            _rb2D.velocity = new Vector2(_moveRightLeft, _rb2D.velocity.y * .5f);
         }
+
+        //if player touches a wall while in the air bounce to the other side
+        if (_touchingWall && !_isGrounded && !_wallJumping)
+        {
+            _wallJumping = true;
+            _rb2D.velocity = new Vector2(-_moveRightLeft * pushOutFromWallForce, _rb2D.velocity.y * 0.5f);
+        }
+
+
     }
+
 }
